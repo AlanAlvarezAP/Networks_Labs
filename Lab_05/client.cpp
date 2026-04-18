7013181bb648b6ede68b7fc96e88bc2cd4004703
AlanAlvarezAP <alan.alvarez@ucsp.edu.pe>
2026-04-18 00:34:11 -0500

    First stable version of ticktacktoe

--- Lab_05/client.cpp
+++ Lab_05/client.cpp
@@ -14,23 +14,23 @@ struct TickTackToe {
 
 void check_win(struct TickTackToe &dat){
     for(int i=0;i<=6;i+=3){
-        if((dat.cell_moves[i] == dat.cell_moves[i+1]) && (dat.cell_moves[i] == dat.cell_moves[i+2])){
+        if((dat.curr_move == dat.cell_moves[i])&&(dat.cell_moves[i] == dat.cell_moves[i+1]) && (dat.cell_moves[i] == dat.cell_moves[i+2])){
             dat.winner_draw='w';
             return;
         }    
     }
 
     for(int i=0;i<=2;i++){
-        if((dat.cell_moves[i] == dat.cell_moves[i+3]) && (dat.cell_moves[i] == dat.cell_moves[i+6])){
+        if((dat.curr_move == dat.cell_moves[i])&&(dat.cell_moves[i] == dat.cell_moves[i+3]) && (dat.cell_moves[i] == dat.cell_moves[i+6])){
             dat.winner_draw='w';
             return;
         }    
     }
-    if((dat.cell_moves[0] == dat.cell_moves[4]) && (dat.cell_moves[0] == dat.cell_moves[8])){
+    if((dat.curr_move == dat.cell_moves[0])&&(dat.cell_moves[0] == dat.cell_moves[4]) && (dat.cell_moves[0] == dat.cell_moves[8])){
         dat.winner_draw='w';
         return;
     }
-    if((dat.cell_moves[2] == dat.cell_moves[4]) && (dat.cell_moves[2] == dat.cell_moves[6])){
+    if((dat.curr_move == dat.cell_moves[2])&&(dat.cell_moves[2] == dat.cell_moves[4]) && (dat.cell_moves[2] == dat.cell_moves[6])){
         dat.winner_draw='w';
         return;
     }            
@@ -39,10 +39,10 @@ void check_win(struct TickTackToe &dat){
 void check_draw(struct TickTackToe &dat){
     for(int i=0;i<9;i++){
         if(dat.cell_moves[i] != '-'){
-            dat.winner_draw='d';
             return;
         }
     }
+    dat.winner_draw='d';
 }
 
 int main() {
@@ -58,15 +58,51 @@ int main() {
 
     connect(sock, (struct sockaddr*)&server, sizeof(server));
 
-    struct TickTackToe data = {{'-','-','-','-','-','-','-','-','-'},'X',false};
+    struct TickTackToe data = {{'-','-','-','-','-','-','-','-','-'},'X','-'};
 
-    while(true){
+    while(data.winner_draw != 'w' && data.winner_draw != 'd'){
+        int cell;
+        do{
+            printf("Give me the cell to play (1-9) ");
+            scanf("%d",&cell);
+        }while((cell <= 0 && cell >= 10) || data.cell_moves[cell-1] != '-');
+
+        data.cell_moves[cell-1]=data.curr_move;
+        check_win(data);
+        check_draw(data);
+
+        data.curr_move='O';
+
         write(sock, (char*)&data, sizeof(data));
         printf("Struct sent\n");
 
 
-    }
+        ssize_t total = 0;
+        ssize_t n;
+        while (total < sizeof(data)) {
 
+            n = read(sock,((char*)&data) + total,sizeof(data) - total);
+
+            if (n <= 0) {
+                printf("Connection closed or error\n");
+                break;
+            }
+
+            total += n;
+        }
+        printf("Received struct:\n");
+        printf("The table status is:\n");
+        for(int i=0;i<9;i+=3){
+            printf("%c ",data.cell_moves[i]);
+            printf("%c ",data.cell_moves[i+1]);
+            printf("%c\n",data.cell_moves[i+2]);
+        }
+        printf("\n");
+        printf("the turn is = %c\n", data.curr_move);
+        printf("the status of the match is = %c\n", data.winner_draw);
+
+
+    }
 
     close(sock);
 
--- Lab_05/server.cpp
+++ Lab_05/server.cpp
@@ -5,10 +5,50 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-struct MyStruct {
-    int id;
-    float value;
+struct TickTackToe {
+    char cell_moves[9];
+    char curr_move;
+    char winner_draw;
 };
+
+void check_win(struct TickTackToe &dat){
+    for(int i=0;i<=6;i+=3){
+        if(dat.curr_move != dat.cell_moves[i]){
+            continue;
+        }
+        if((dat.cell_moves[i] == dat.cell_moves[i+1]) && (dat.cell_moves[i] == dat.cell_moves[i+2])){
+            dat.winner_draw='w';
+            return;
+        }    
+    }
+
+    for(int i=0;i<=2;i++){
+        if(dat.curr_move != dat.cell_moves[i]){
+            continue;
+        }
+        if((dat.cell_moves[i] == dat.cell_moves[i+3]) && (dat.cell_moves[i] == dat.cell_moves[i+6])){
+            dat.winner_draw='w';
+            return;
+        }    
+    }
+    if((dat.curr_move == dat.cell_moves[0])&&(dat.cell_moves[0] == dat.cell_moves[4]) && (dat.cell_moves[0] == dat.cell_moves[8])){
+        dat.winner_draw='w';
+        return;
+    }
+    if((dat.curr_move == dat.cell_moves[2])&&(dat.cell_moves[2] == dat.cell_moves[4]) && (dat.cell_moves[2] == dat.cell_moves[6])){
+        dat.winner_draw='w';
+        return;
+    }            
+}
+
+void check_draw(struct TickTackToe &dat){
+    for(int i=0;i<9;i++){
+        if(dat.cell_moves[i] != '-'){
+            return;
+        }
+    }
+    dat.winner_draw='d';
+}
 
 int main() {
 
@@ -30,26 +70,49 @@ int main() {
 
     client_fd = accept(server_fd, (struct sockaddr*)&addr, &addrlen);
 
-    struct MyStruct data;
+    struct TickTackToe data;
 
-    ssize_t total = 0;
-    ssize_t n;
+    do{
+        ssize_t total = 0;
+        ssize_t n;
+        while (total < sizeof(data)) {
 
-    while (total < sizeof(data)) {
+            n = read(client_fd,((char*)&data) + total,sizeof(data) - total);
 
-        n = read(client_fd,((char*)&data) + total,sizeof(data) - total);
+            if (n <= 0) {
+                printf("Connection closed or error\n");
+                break;
+            }
 
-        if (n <= 0) {
-            printf("Connection closed or error\n");
-            break;
+            total += n;
         }
+        printf("Received struct:\n");
+        printf("The table status is:\n");
+        for(int i=0;i<9;i+=3){
+            printf("%c ",data.cell_moves[i]);
+            printf("%c ",data.cell_moves[i+1]);
+            printf("%c\n",data.cell_moves[i+2]);
+        }
+        printf("\n");
+        printf("the turn is = %c\n", data.curr_move);
+        printf("the status of the match is = %c\n", data.winner_draw);
 
-        total += n;
-    }
+        int cell;
+        do{
+            printf("Give me the cell to play (1-9) ");
+            scanf("%d",&cell);
+        }while((cell <= 0 && cell >= 10) || data.cell_moves[cell-1] != '-');
 
-    printf("Received struct:\n");
-    printf("id = %d\n", data.id);
-    printf("value = %f\n", data.value);
+        data.cell_moves[cell-1]=data.curr_move;
+        check_win(data);
+        check_draw(data);
+        data.curr_move='X';
+
+        write(client_fd, (char*)&data, sizeof(data));
+        printf("Struct sent\n");
+
+    }while(data.winner_draw != 'w' && data.winner_draw != 'd');
+
 
     close(client_fd);
     close(server_fd);
