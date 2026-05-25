39920d590add0fc242e0391f1edcac8385b34367
PaquitoAP <144193511+AlanAlvarezAP@users.noreply.github.com>
2026-05-25 12:35:19 -0500

    Update DataStructure_UDP.hpp

--- Lab_07/DataStructure_UDP.hpp
+++ Lab_07/DataStructure_UDP.hpp
@@ -516,7 +516,7 @@ public:
 			    auto elapsed=std::chrono::duration_cast<std::chrono::milliseconds>(now-starting).count();
 			    if(elapsed > 1000){
 			        std::cout << "TIMEOUT -> retransmitting fragment " << i+1<< std::endl;
-					sendto(client_socket,packet.data(),500,0,(sockaddr*)&server_addr,sizeof(server_addr));
+					sendto(client_socket,sf.packets[i].data(),500,0,(sockaddr*)&server_addr,sizeof(server_addr));
 			        starting = std::chrono::steady_clock::now();
 			    }
 			}
