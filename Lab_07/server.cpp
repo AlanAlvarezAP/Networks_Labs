f29e2c087e9b3c0881ceb450cf2b292f4f5ae9a7
PaquitoAP <144193511+AlanAlvarezAP@users.noreply.github.com>
2026-05-25 09:00:23 -0500

    Update server.cpp

--- Lab_07/server.cpp
+++ Lab_07/server.cpp
@@ -47,8 +47,12 @@ void read_thread_UDP(int SocketFD){
         }
 
         buffer[n] = '\0';
-
-        sv_UDP.Cases_Server(buffer[0],buffer, SocketFD, sender);
+        if(buffer[8]=='F'){
+            sv_UDP.Cases_Server(buffer[8],buffer, SocketFD, sender);
+        }else{
+            sv_UDP.Cases_Server(buffer[0],buffer, SocketFD, sender);
+        }
+        
     }
 }
 
