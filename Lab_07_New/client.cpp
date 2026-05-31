a2c7ea0d6cbdf295c67d307576b60c191046d654
PaquitoAP <144193511+AlanAlvarezAP@users.noreply.github.com>
2026-05-31 12:37:08 -0500

    fix(client.cpp): Fix the client main only UDP

--- Lab_07_New/client.cpp
+++ Lab_07_New/client.cpp
@@ -1,16 +1,7 @@
 /* Client code in C */ 
-#include "DataStructure.hpp"
 #include "DataStructure_UDP.hpp"
 
-Client_Protocols clp_TCP;
 Client_Protocols_UDP clp_UDP;
-
-enum TypeConnection{
-    TCP=1,
-    UDP
-};
-
-TypeConnection connection_type;
 
 void print_menu() {
     std::cout << "===================================" << std::endl;
@@ -52,34 +43,11 @@ char Cast_Option(int option){
 	}
 }
 
-void read_thread_TCP(int n,int SocketFD){
-    char buffer;
-    while (clp_TCP.running) {
-        n = read(SocketFD, &buffer, 1);
-        if(n == 0){
-            std::cout << "Server closed connection because of logout :D " << std::endl;
-            close(SocketFD);
-	        clp_TCP.running=false;
-	        clp_TCP.logging_status=false;
-            break;
-        }
-        else if (n < 0) {
-            std::cout << "Disconection from server because of an ERROR..." << std::endl;
-            close(SocketFD);
-	        clp_TCP.running=false;
-	        clp_TCP.logging_status=false;
-            break;
-        }
-        clp_TCP.Cases_Client(buffer, n, SocketFD);
-    }
-     
-}
-
 void read_thread_UDP(int SocketFD){
     char buffer[500];
     sockaddr_in sender;
     socklen_t len=sizeof(sender);
-    while(true){
+    while(clp_UDP.running){
         int n=recvfrom(SocketFD,buffer,sizeof(buffer),0,(sockaddr*)&sender,&len);
 
         if(n<=0){
@@ -97,22 +65,10 @@ void read_thread_UDP(int SocketFD){
 }
 
 int main(void){
-    int option;
-
-    std::cout << "What type of connection you want? " << std::endl;
-    std::cout << "1.TCP" << std::endl;
-    std::cout << "2.UDP" << std::endl;
-    std::cin >> option;
-
-    connection_type=(option==1)?TCP:UDP;
 
     struct sockaddr_in stSockAddr;
     int SocketFD;
-    if(connection_type == TCP){
-        SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    }else{
-        SocketFD = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    }
+    SocketFD = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
     
     int n;
 
@@ -121,41 +77,29 @@ int main(void){
     stSockAddr.sin_family = AF_INET;
     stSockAddr.sin_port = htons(45000);
     inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
- 
-    if(connection_type == TCP){
-        connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in));
-		clp_TCP.running = true;
-        std::thread(read_thread_TCP,n,SocketFD).detach();    
-    }else{
-		clp_UDP.running = true;
-        std::thread(read_thread_UDP,SocketFD).detach();
-    }
-    
 
-    clp_TCP.running = true;
-    clp_UDP.running = true;
+	clp_UDP.running = true;
+	std::thread(read_thread_UDP,SocketFD).detach();
+    
     print_menu();
 
-    
-    while((clp_TCP.running || clp_UDP.running)) {
+    while(clp_UDP.running) {
         std::cout << "SELECT AN ACTION :D " << std::endl;
         int action;
         std::cin >> action;
 	    std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         char option = Cast_Option(action);
-        if (option != 'L' && (clp_TCP.logging_status == false && clp_UDP.logging_status == false)) {
+        if (option != 'L' && clp_UDP.logging_status == false) {
             std::cout << "You are not logged in, try logging pls :D" << std::endl;
             print_menu();
             continue;
         }
 
-        if(connection_type == TCP){
-            clp_TCP.Cases_Client(option,n,SocketFD);
-        }else{
-            clp_UDP.Cases_Client_UDP(option,std::string{option},SocketFD,stSockAddr);
-        }
+        clp_UDP.Cases_Client_UDP(option,std::string{option},SocketFD,stSockAddr);
         
     }
     std::cout << " LEAVING ... " << std::endl;
+	clp_UDP.running = false;
+	close(SocketFD);
     return 0;
 }
